642834fac8f63afbfc6f1ba437e469bdba73a262
Marycorreia12 <correia_nety@hotmail.com>
2022-07-21 17:44:17 +0100

    footer css modules update

--- ebike/src/components/Footer/Footer.js
+++ ebike/src/components/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {ReactComponent as Iconic} from '../../images/Iconic-ebikes.svg';
 import SocialMedia from '../SocialMedia/SocialMedia';
 import { Link } from 'react-router-dom'
-import './footer.css'
+import styles from './Footer.module.css'
 import MasterCard from '../../images/mastercard.svg'
 import VisaEletron from '../../images/visa-electron.svg'
 import Paypal from '../../images/paypal.svg'
@@ -12,71 +12,72 @@ import EmailSubscribe from '../EmailSubscribe/EmailSubscribe';
 
 const Footer = () => {
   return (
-    <footer id='footer'>
-        <section className='footer-container-1 container'>
-            <div className='footer-left'>
+    <footer className="container">
+        <div className={styles.wrapper}>
+        <section className={styles.content_1}>
+            <div className={styles.content_1_left}>
                 <Iconic/>
-                <div className='footer-left-input'>
+                <div className={styles.subscribe_input}>
                     <h4>SUBSCRIBE</h4>
                     <p>Subscribe us and you won't miss the new arrivals, as well as discounts and sales.</p>
                     <EmailSubscribe/>
                 </div>
-                <div className='footer-left-icons'>
+                <div className={styles.icons}>
                     <h4>STAY IN TOUCH</h4>
                     <SocialMedia color= '#fff' background='#8DC63F' padding='4px'/>
-                    <p className='p'>Questions? Please write us at: ABC@gmail..com</p>
+                    <p>Questions? Please write us at: ABC@gmail.com</p>
                 </div>
             </div>
-            <div  className='footer-right'>
-                <div className='footer-right-nav'>
+            <div className={styles.content_1_right}>
+                <div className={styles.content_1_right_navbar}>
                     <h4>INFORMATION</h4>
-                    <ul className='nav-content'>
-                        <li className='nav-items-f'>
+                    <ul className={styles.links_content}>
+                        <li className={styles.link}>
                         <Link to="/about">About Us</Link>
                         </li>
-                        <li className='nav-items-f'>
+                        <li className={styles.link}>
                         <Link to="/services">Services</Link>
                         </li>
-                        <li className='nav-items-f'>
+                        <li className={styles.link}>
                         <Link to="/shops">Shops</Link>
                         </li>
-                        <li className='nav-items-f'>
+                        <li className={styles.link}>
                         <Link to="/gallery">Gallery</Link>
                         </li>
-                        <li className='nav-items-f'>
+                        <li className={styles.link}>
                         <Link to="/news">News</Link>
                         </li>
-                        <li className='nav-items-f'>
+                        <li className={styles.link}>
                         <Link to="/contact">Contact Us</Link>
                         </li>    
-                </ul>
+                    </ul>
                 </div>
                 <div className='footer-right-nav'>
                     <h4>SHOP</h4>
-                    <ul className='nav-content'>
-                        <li className='nav-items-f'>
+                    <ul className={styles.links_content}>
+                        <li className={styles.link}>
                         <Link to="">Road Bike</Link>
                         </li>
-                        <li className='nav-items-f'>
+                        <li className={styles.link}>
                         <Link to="">Mountain Bike</Link>
                         </li>
-                        <li className='nav-items-f'>
+                        <li className={styles.link}>
                         <Link to="">BMX bike</Link>
                         </li>
-                        <li className='nav-items-f'>
+                        <li className={styles.link}>
                         <Link to="">City Bike</Link>
                         </li>
-                        <li className='nav-items-f'>
+                        <li className={styles.link}>
                         <Link to="">Kids Bike</Link>
                         </li>   
                     </ul>
                 </div>
             </div>
         </section>
-        <section className='footer-container-2 container'>
-            <div className='footer-copyright'>
-                <p className='copyright'>ICONIC<span> © 2022. All Rights Reserved.</span></p>
-                <div className='credit-cards'>
+        <section className={styles.content_2}>
+            <div className={styles.content_2_copyright}>
+                <p className={styles.copyright}>ICONIC<span className={styles.copyright_link}> © 2022. All Rights Reserved.</span></p>
+                <div className={styles.credit_cards}>
                     <img src={MasterCard} alt="MasterCard" style={{cursor: 'pointer'}} />
                     <img src={VisaEletron} alt="VisaEletron" style={{cursor: 'pointer'}}/>
                     <img src={Paypal} alt="Paypal" style={{cursor: 'pointer'}}/>
@@ -84,6 +85,9 @@ const Footer = () => {
                 </div>
             </div>
         </section>
+
+        </div>
+        
     </footer>
     
   )
--- ebike/src/components/SocialMedia/SocialMedia.js
+++ ebike/src/components/SocialMedia/SocialMedia.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 const SocialMedia = ({color, background, padding}) => { 
   return ( 
-    <div style={{display: 'flex', gap: '1.6rem', cursor: 'pointer'}}>
+    <div style={{display: 'flex', gap: '1.6rem', cursor: 'pointer', marginBottom: '1rem'}}>
         <FontAwesomeIcon icon={['fab', 'facebook']} style={{color: color, background: background, padding: padding}}/>
         <FontAwesomeIcon icon={['fab', 'instagram']} style={{color: color, background: background, padding: padding}}/>
         <FontAwesomeIcon icon={['fab', 'youtube']} style={{color: color, background: background, padding: padding}}/>
