65f664fa8fdd40f3f0911cf9564701a7aa424121
Marycorreia12 <correia_nety@hotmail.com>
2022-07-22 16:14:02 +0100

    cart page

--- ebike/src/components/Cart/Cart.js
+++ ebike/src/components/Cart/Cart.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from './Cart.module.css'
 
 const Cart = () => {
   return (
-    <div>
-      Your Shopping Cart is Empty
-      <Link to="/wishlist">WishList</Link>
+    <div className='container'>
+      <h4 className={styles.title}>Shopping Cart</h4>
+      <div className={styles.wrapper}>
+        <p className={styles.subtitle}>Your Shopping Cart is Empty</p>
+        <small>Nothin' to see here.</small>
+        <small>Check out your wishlist items.</small>
+        <Link to="/wishlist" className={styles.btn}>View Wishlist Items</Link>
+      </div>
     </div>
   )
 }
